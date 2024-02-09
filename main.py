bdd1d1a170a34940a87c1e8380d3376f84d41176
SAMUEL BOLARIN <63271806+samuelbol@users.noreply.github.com>
2024-02-09 03:27:51 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
+from keep_alive import keep_alive
+import pytz
+import requests
 from apscheduler.schedulers.blocking import BlockingScheduler
 from bs4 import BeautifulSoup
-from keep_alive import keep_alive
-from pytz import utc
-import requests
+from pymongo import MongoClient
 import os
+
+connection_string = os.environ.get("connection_string")
+client = MongoClient(connection_string)
+
+# Replace 'your_database_name' and 'your_collection_name' with actual names
+db = client['chelsea_news']
+collection = db['caught_offside']
 
 HEADER = {
     "User-Agent":
-    "Mozilla/5.0 (X11; CrOS x86_64 8172.45.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.64 Safari/537.36"
+        "Mozilla/5.0 (X11; CrOS x86_64 8172.45.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.64 Safari/537.36"
 }
 
-BOT_TOKEN = os.environ.get('bot_token')
-CHAT_ID = os.environ.get('chat_id')
+BOT_TOKEN = os.environ.get("bot_token")
+CHAT_ID = os.environ.get("chat_id")
 BASE_URL = f"https://api.telegram.org/bot{BOT_TOKEN}/"
 
 keep_alive()
+nigerian_tz = pytz.timezone("Africa/Lagos")
 
 
 def scrape_caught_off_chls():
@@ -28,25 +37,17 @@ def scrape_caught_off_chls():
         "div", class_="col-s-12 col-m-6 post-container post-standard")
     news_list = []
 
-    for news_card in news_cards[:5]:
+    for news_card in news_cards[:10]:
         crd_title = news_card.find("div",
                                    class_="img-txt c-std-mgn-b std-pad").a.text
         crd_img = news_card.find("picture").img.get('data-src', '')
 
         crd_link = news_card.find("div",
                                   class_="img-txt c-std-mgn-b std-pad").a.get(
-                                      'href', '')
+            'href', '')
 
         if not crd_title or not crd_img or not crd_link:
             continue
-
-        try:
-            with open("logfile.txt", "r") as file:
-                saved_titles = [line.rstrip("\n") for line in file.readlines()]
-                if crd_title in saved_titles:
-                    continue
-        except FileNotFoundError:
-            pass
 
         resp = requests.get(f"https://www.caughtoffside.com{crd_link}",
                             headers=HEADER,
@@ -55,9 +56,9 @@ def scrape_caught_off_chls():
         soup = BeautifulSoup(resp.content, "html.parser")
 
         card_contd = (soup.find("div", {"id": "article-body"})).find_all("p")
-        card_desc = "".join([
-            content.get_text(strip=True) + '\n\n' for content in card_contd[:3]
-        ])
+        card_desc = card_contd[0].get_text() + "\n"
+        card_desc += card_contd[1].get_text()
+
         news_list.append({
             "title": crd_title,
             "image": crd_img,
@@ -78,17 +79,12 @@ def send_news_to_telegram(article_items):
         if not title_ or not story_:
             continue
 
-        message = f"🚨 *{title_}*\n\n{story_}\n" \
-                  f"🔗 *CaughtOffside*\n\n" \
+        message = f"🚨 *{title_}*\n\n{story_}\n\n" \
+                  f"_(via CaughtOffside)_\n\n" \
                   f"📲 @JustCFC"
-        # print(message)
-        try:
-            with open("logfile.txt", "r", encoding='utf-8') as file:
-                saved_titles = [line.rstrip("\n") for line in file.readlines()]
-        except FileNotFoundError:
-            saved_titles = []
 
-        if title_ not in saved_titles:
+        saved_titles = collection.find_one({"text": title_})
+        if not saved_titles:
             response = requests.post(BASE_URL + "sendPhoto",
                                      json={
                                          "chat_id": CHAT_ID,
@@ -101,11 +97,7 @@ def send_news_to_telegram(article_items):
             if response.status_code == 200:
                 print("Message sent successfully.")
 
-                with open("logfile.txt", "a", encoding='utf-8') as file:
-                    file.write(f"{title_}\n")
-
             else:
-                print(response.text)
                 print(
                     f"Message sending failed. Status code: {response.status_code}"
                 )
@@ -116,7 +108,7 @@ def main():
     send_news_to_telegram(news_items)
 
 
-scheduler = BlockingScheduler(timezone=utc)
-scheduler.add_job(main, "interval", minutes=5)
-# scheduler.start()
-main()
+scheduler = BlockingScheduler(timezone=nigerian_tz)
+scheduler.add_job(main, "interval", minutes=30)
+scheduler.start()
+# main()
